b63a5057de94c3deae3b23b0810e2c181a5be043
eccyan <g00.eccyan@gmail.com>
2020-04-23 16:22:17 +0900

    Fix format string

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
+#include <fmt/format.h>
 
 int main(int argc, char** argv) {
   std::string who = "Unknown";
   if (argc > 1) {
     who = argv[1];
   }
-  std::cout << "Welcome to tsumu, " + who + " !" << std::endl;
+  std::cout << fmt::format("Welcome to tsumu, {0} !", who) << std::endl;
   return 0;
 }
