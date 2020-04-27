e1131b12af180371a03f44edf14611aa65325038
eccyan <g00.eccyan@gmail.com>
2020-04-27 22:24:30 +0900

    Use added functions in main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,5 @@
+#include "socket.hh"
+
 #include <cstdio>
 #include <cstring>
 #include <cerrno>
@@ -21,37 +23,13 @@ int main(int argc, char** argv) {
   }
   std::cout << "Welcome to tsumu !" << std::endl;
 
-  const int fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (fd == -1) {
-    std::cerr << fmt::format("Error creating new socket: {0}", strerror(errno)) << std::endl;
-    exit(EXIT_FAILURE);
-  }
+  auto socket_factory = std::unique_ptr<tsumu::socket_factory>(new tsumu::socket_factory(AF_INET, SOCK_STREAM, 0));
+  auto [ sock, err ] = socket_factory->make_socket(port);
 
-  int opt = 1;
-  if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
-  {
-    std::cerr << fmt::format("Error setting socket: {0}", strerror(errno)) << std::endl;
-    exit(EXIT_FAILURE);
-  }
+  sock->configure_and_bind_tcp();
+  sock->listen(3);
 
-  struct sockaddr_in address;
-  address.sin_family = AF_INET;
-  address.sin_addr.s_addr = INADDR_ANY;
-  address.sin_port = htons(port);
-  if (bind(fd, (struct sockaddr *)&address, sizeof(address)) == -1)
-  {
-    std::cerr << fmt::format("Error bind socket: {0}", strerror(errno)) << std::endl;
-    exit(EXIT_FAILURE);
-  }
-  if (listen(fd, 3) == -1)
-  {
-    std::cerr << fmt::format("Error listen socket: {0}", strerror(errno)) << std::endl;
-    exit(EXIT_FAILURE);
-  }
-
-
-  int addrlen = sizeof(address);
-  const int socket = accept(fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+  const int socket = sock->accept();
   if (socket == -1)
   {
     std::cerr << fmt::format("Error accept socket: {0}", strerror(errno)) << std::endl;
